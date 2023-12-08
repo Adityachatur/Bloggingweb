4d4eb74329859da73accb45fcfef8fb2a078d1f8
Adityachatur <66326241+Adityachatur@users.noreply.github.com>
2023-12-08 12:51:35 +0530

    First coomit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
+// import Blogs from './Components/Pages/Blogs';
+import About from './Components/Pages/About';
+import Home from './Components/Pages/Home';
+import Navbar from './Components/Navbar/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Footer from './Components/Navbar/Footer';
+import DetailBlog from './Components/Pages/DetailBlog';
+import Service from './Components/Pages/Service';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Service' element={<Service/>}/>
+        <Route
+          path='/blogs/:id'
+          element={<DetailBlog />}
+          loader={(params) => fetch(`http://localhost:5000/blogs/${params.id}`).then((res) => res.json())}
+        />
+
+      </Routes>
+      <Footer/>
+
+    </>
   );
 }
 
